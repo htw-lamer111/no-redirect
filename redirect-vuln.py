2653128c6bd60716a0be1df30f9378d281c05cb9
htw-lamer <124086931+htw-lamer111@users.noreply.github.com>
2023-02-11 06:18:04 -0600

    Update redirect-vuln.py

--- redirect-vuln.py
+++ redirect-vuln.py
@@ -4,6 +4,7 @@ from colorama import Back
 from colorama import Style
 import time
 from bs4 import BeautifulSoup
+import string
 try:
 	init()
 	print(Fore.GREEN, "="*100)
@@ -35,24 +36,22 @@ try:
 	NN  N O   O     R   R E    D  D  I  R   R E    C      TT   
 	N N N O   O --- RRRR  EEE  D  D  I  RRRR  EEE  C      TT   
 	N  NN O   O     R R   E    D  D  I  R R   E    C      TT   
-	N   N  OOO      R  RR EEEE DDD  III R  RR EEEE  CCC   TT 
-	
-	github: https://github.com/htw-lamer111	                                                          
+	N   N  OOO      R  RR EEEE DDD  III R  RR EEEE  CCC   TT   
+	github: https://github.com/htw-lamer111                                               
 			""")
 	newurl = []
 	print("url example: https://site/admin/login.php")
 	bebroid = input("site url:")
 	file = open("list.txt", "r", encoding="utf-8")
-	if 'login.php' in bebroid:
-		bebroid = bebroid.replace("login.php", "", 1) 
+	bebroid1 = bebroid.replace( 'login.php', '')
 	onstring = file.read().split("\n")[:-1]
 	for amongos in onstring:
-	    end_url = bebroid+amongos	    		
-	    get = requests.get(bebroid+amongos, allow_redirects=False)
+	    end_url = bebroid1+amongos	    		
+	    get = requests.get(bebroid1+amongos, allow_redirects=False)
 	    if get.status_code == 200:
-	    	print("VULNERABILITY MAYBE FOUND, ENTER WITH CYBERFOX \nAND ENABLE NO REDIRECT:", end_url)
+	    	print( "[", Fore.GREEN, "YES",Fore.GREEN,"]: ",  "  status_code:", end_url)
 	    else:
-	    	print("VULNERABILITY NOT FOUND", end_url)
+	    	print("[", Fore.RED, "NO",Fore.GREEN,"]: ", end_url,  "   status code:", get.status_code)
 except KeyboardInterrupt:
 	print("\nGood bye....")
 except requests.exceptions.MissingSchema:
